37363b8ed37972f563c227eaab56302eca5b9fa9
DylanSuarez <2000sajo@gmail.com>
2023-07-27 12:41:34 -0500

    bcryptHandle

---
+++ src/utils/bcryptHandler.ts
@@ -0,0 +1,5 @@
+import { hash, compare } from "bcryptjs"
+
+export const encrypt = async (password: string) => await hash(password, 8)
+
+export const comparePass = async (password: string, passwordHash: string) => await compare(password, passwordHash)
