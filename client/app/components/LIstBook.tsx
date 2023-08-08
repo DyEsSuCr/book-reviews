3f232f2344f80b1100bcf52f47036d8464196ded
DylanSuarez <2000sajo@gmail.com>
2023-08-08 18:22:27 -0500

    pnpm lock

--- client/app/components/LIstBook.tsx
+++ client/app/components/LIstBook.tsx
@@ -1,5 +1,4 @@
 import { type ListBook } from '@/interfaces/book.interface'
-import Image from 'next/image'
 
 export async function fetchBooks () {
   const res = await fetch('https://www.googleapis.com/books/v1/volumes?q=title=hola&maxResult=5')
@@ -15,7 +14,7 @@ export async function ListBooks () {
       {books.items.map(book => (
         <figure key={book.id}>
           <h2 className='text-yellow-700'>{book.volumeInfo.title}</h2>
-          <Image width={200} height={200} src={book.volumeInfo.imageLinks?.thumbnail ?? 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png'} alt={book.volumeInfo.title} />
+          <img className='object-cover w-52 h-52' src={book.volumeInfo.imageLinks?.thumbnail ?? 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png'} alt={book.volumeInfo.title} />
           <p>{book.volumeInfo.description?.split('.')[0]}</p>
           <a href={book.selfLink} target='_blank' rel='noreferer noreferrer' className='text-red-600'>Mas info</a>
         </figure>
