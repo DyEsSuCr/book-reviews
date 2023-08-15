7b4fbb1201ade8668842b4c14ca121977a5efe4a
DylanSuarez <2000sajo@gmail.com>
2023-08-15 11:33:47 -0500

    schemaValidator

--- src/middlewares/schemaValidator.ts
+++ src/middlewares/schemaValidator.ts
@@ -1,14 +1,16 @@
 import { ErrorMessajes } from '@/interfaces/errors.interface'
 import { response, responseErr } from '@/utils'
 import { type NextFunction, type Request, type Response } from 'express'
-import { ZodError, type AnyZodObject } from 'zod'
+import { type AnyZodObject } from 'zod'
 
 export const schemaValidator = (schema: AnyZodObject) => ({ body, params, query }: Request, res: Response, next: NextFunction) => {
-  try {
-    schema.parse({ body, params, query })
-    next()
-  } catch (err) {
-    if (err instanceof ZodError) return response(res, 400, err.issues.map((issue) => ({ messaje: issue.message })))
+  const result = schema.safeParse({ body, params, query })
+
+  if (!result.success) {
+    if (result.error) return response(res, 400, result.error.issues.map((issue) => ({ messaje: issue.message })))
+
     return responseErr(res, ErrorMessajes.INTERNAL_SERVER_ERROR)
   }
+
+  next()
 }
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,16 +1,36 @@
 import { z } from 'zod'
 
 export const userSignupSchema = z.object({
+
   body: z.object({
-    username: z.string().nonempty().min(3, 'min length 3').max(10, 'max length 10'),
-    password: z.string().nonempty().min(6, 'min length 6').max(30, 'max length 30'),
-    email: z.string().nonempty().email()
+    username: z.string()
+      .nonempty({ message: 'Username is required' })
+      .min(3, 'min length 3')
+      .max(10, 'max length 10'),
+
+    password: z.string()
+      .nonempty({ message: 'Password is required' })
+      .min(6, 'min length 6')
+      .max(30, 'max length 30'),
+
+    email: z.string()
+      .nonempty({ message: 'Email is required' })
+      .email()
   })
+
 })
 
 export const userSigninSchema = z.object({
+
   body: z.object({
-    password: z.string().nonempty().min(6, 'min length 6').max(30, 'max length 30'),
-    email: z.string().nonempty().email()
+    password: z.string()
+      .nonempty({ message: 'Password is required' })
+      .min(6, 'min length 6')
+      .max(30, 'max length 30'),
+
+    email: z.string()
+      .nonempty({ message: 'Email is required' })
+      .email()
   })
+
 })
